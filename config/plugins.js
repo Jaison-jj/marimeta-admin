028625d48dd10a23cb4fc6f992f89a7ceb07018d
Jaison John <jaison.john@ileafsolutions.com>
2024-10-16 18:09:29 +0530

    feature/ send email

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    email: {
+      config: {
+        provider: 'nodemailer',
+        providerOptions: {
+          host: env('SMTP_HOST', 'smtp.gmail.com'),
+          port: env('SMTP_PORT', 587),
+          auth: {
+            user: 'jaison.john@ileafsolutions.com',
+            pass: 'zmdn lrpd gged wvlq',
+          },
+        },
+        settings: {
+          defaultFrom: 'hello@example.com',
+          defaultReplyTo: 'hello@example.com',
+        },
+      },
+    },
+  });
--- src/api/home-page/controllers/home-page.js
+++ src/api/home-page/controllers/home-page.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  * home-page controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+// @ts-ignore
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home-page.home-page');
+module.exports = createCoreController(
+  "api::home-page.home-page",
+  ({ strapi }) => ({
+    async customAction(ctx) {
+      try {
+        await strapi.plugin('email').service('email').send({
+            to: 'jaison.john@ileafsolutions.com',
+            from: 'jaison.john@ileafsolutions.com',
+            subject: 'Hello world',
+            text: 'Hello world',
+            html: `<h4>Hello world</h4>`,
+          })
+          ctx.send({ message: "Email sent successfully" });
+      } catch (error) {
+        ctx.body = error;
+      }
+    },
+  })
+);
